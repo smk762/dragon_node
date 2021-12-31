d892b63b4f2aba41b038383e7739755fb103dce3
smk762 <smk762@iinet.net.au>
2021-12-31 14:04:33 +0800

    find port

--- refresh_non_antara_wallets.py
+++ refresh_non_antara_wallets.py
@@ -1,5 +1,14 @@
 #!/usr/bin/env python3
 from lib_tui import refresh_non_antara_wallet
 
+max_tx_count = 2000
 for coin in ["CHIPS", "EMC2", "AYA", "GLEEC-OLD", "SFUSD"]:
-	refresh_non_antara_wallet(coin)
+    try:
+        tx_count = lib_rpc.get_wallet_tx_count(coin)
+        if tx_count > max_tx_count:
+            refresh_non_antara_wallet(coin)
+        else:
+            print(f"Skipping {coin}, less than {max_tx_count}")
+    except requests.exceptions.RequestException as e:
+        print(f"{coin} not responding, skipping...")
+	
