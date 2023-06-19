7a6bd9b77fb2565ad0ef9d346eca8b90e8c70572
smk762 <smk762@iinet.net.au>
2023-06-19 19:10:37 +0800

    sort coins list

--- stats.py
+++ stats.py
@@ -87,6 +87,7 @@ class StatsLine:
 class Stats:
     def __init__(self, coins: list):
         self.coins = coins
+        self.coins.sort()
         self.col_widths = [12, 6, 8, 6, 10, 10, 8, 6, 8, 8, 8]
         self.columns = [
             "COIN", "NTX", "LASTNTX", "UTXO", "BALANCE",
