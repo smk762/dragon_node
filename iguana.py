1023aefbfc71070be8dc25277c450857351a60c0
smk762 <smk762@iinet.net.au>
2023-07-02 13:04:36 +0800

    remove pipe logs

--- iguana.py
+++ iguana.py
@@ -30,12 +30,10 @@ class Iguana():
         else:
             bin = const.IGUANA_BIN_3P
         try:
-            logfile = open(const.IGUANA_LOGS[self.server], "w")
-            iguana_path = f"{const.DPOW_PATH}/iguana"
             subprocess.Popen(
-                [bin], start_new_session=True,
-                universal_newlines=True, cwd=iguana_path,
-                stdout=logfile, stderr=logfile, shell=True
+                [bin],
+                start_new_session=True,
+                universal_newlines=True
             )
         except subprocess.CalledProcessError as e:
             logger.error(e)
